ba0e72b98770f87f7f4f5afa94280dca2df954c7
pkarthikbit <pkarthikbit@gmail.com>
2019-11-28 20:54:32 +0100

    boot logo main time display when exit selected alarm status display did implemented for wr time, alarm, set/ clr alarm

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -17,16 +17,17 @@ String disp_menu[NANOCAN_MENUCOUNT] =
   "<-Exit"
 };
 
-byte disp_submenu[60], req_CmnTime[3], req_time[3], req_alarm[3];
+byte disp_submenu[60], req_CmnTime[3], req_time[3], req_alarm[3], rcd_time[3]= {11, 12, 13};
 byte NANOCAN_SUBMENUDGTCNT[3] = {24, 60, 60};
 
 int menu_sel = -1, submenu_sel = -1, submenuopt_sel = 1;
-bool submenuopt_bypass = false;
+bool submenuopt_bypass = false, alarm_st = false;
 char clk_HHMMSS[3];
 
 timer_struct timer_pressSwt;
 timer_struct timer_nanoCANMenudisp;
 timer_struct timer_nanoCANSubMenudisp;
+timer_struct timer_mainclk;
 
 /****************************************************************************************************/
 // Declaration for OLED
@@ -37,8 +38,10 @@ MCP2515 mcp2515(10);
 struct can_frame rx_canMsg;
 struct can_frame tx_canMsg;
 
-timer_struct timer_0xF101_req;
-timer_struct timer_0xF101_resp;
+timer_struct timer_0xF101_req, timer_0xF101_resp;
+timer_struct timer_0xF101Wr_req, timer_0xF101Wr_resp;
+timer_struct timer_0xF102_req, timer_0xF102_resp;
+timer_struct timer_0xF103_req, timer_0xF103_resp;
 
 byte retval = MCP2515::ERROR_OK;
 boolean tstr_req = FALSE;     //Flag to check for P2 timer. P2* = P2 and no NRC78.
@@ -148,7 +151,7 @@ void nanoCAN_Menu(int rot_key)
       j = menu_sel; 
       k = 0;
     }
-  
+
     display.clearDisplay();
     display.setTextSize(1);
     display.setCursor(0, 0);
@@ -166,6 +169,45 @@ void nanoCAN_Menu(int rot_key)
     
     stop_timer(&timer_nanoCANMenudisp);
   }
+  else
+  {
+    start_timer(&timer_mainclk);
+
+    if((get_timer(&timer_mainclk) > 1000) &&
+        (menu_sel == NANOCAN_MENUCOUNT -1))
+    {
+      display.clearDisplay();
+      display.setTextSize(3);
+
+      for(byte x=0; x < 3; x++)
+      {
+        display.setCursor((0 + (40 * x)), 00);
+        display.setTextColor(WHITE);
+        display.print(rcd_time[x]);
+        if(x != 2)
+        {
+          display.print(":");
+        }
+      }
+
+      display.setTextSize(1);
+      display.setCursor(30, 25);
+      display.setTextColor(BLACK, WHITE);
+      if(alarm_st)
+      {
+        display.print("Alarm On");
+      }
+      else
+      {
+        display.print("Alarm Off");
+      }
+      
+
+      display.display();
+      stop_timer(&timer_mainclk);
+    }
+  }
+  
 }
 
 /***************************************************************************************************/
@@ -276,14 +318,13 @@ void RDBI_0xF101()
       {
         if(rx_canMsg.data[1] == (tx_canMsg.data[1] + 0x40))
         {
-          if((rx_canMsg.data[0] == 0x07) &&
+          if((rx_canMsg.data[0] == 0x06) &&
               ((((rx_canMsg.data[2] << 8) & 0xFF00) | 
                 ((rx_canMsg.data[3] << 0) & 0x00FF) ) == 0xF101))
           {
-            //Serial.println(((rx_canMsg.data[4] << 24) & 0xFF000000) | 
-                           // ((rx_canMsg.data[5] << 16) & 0x00FF0000) | 
-                           // ((rx_canMsg.data[6] << 8)  & 0x0000FF00) | 
-                           // ((rx_canMsg.data[7] << 0)  & 0x000000FF));
+            rcd_time[0] = rx_canMsg.data[4];
+            rcd_time[1] = rx_canMsg.data[5];
+            rcd_time[2] = rx_canMsg.data[6];
           }        
         }
       }
@@ -293,6 +334,163 @@ void RDBI_0xF101()
   {
     tstr_req = FALSE;
     stop_timer(&timer_0xF101_resp);   
+  }
+}
+
+
+/***************************************************************************************************/
+//WDBI - 0xF101
+void WDBI_0xF101()
+{
+  //Request - CPU time - every 2000 ms
+  start_timer(&timer_0xF101Wr_req);
+  if(((get_timer(&timer_0xF101Wr_req) > 2000) ||
+        (retval != MCP2515::ERROR_OK)) &&
+        (tstr_req == FALSE))
+  {
+    tx_canMsg.data[0] = 0x06;         
+    tx_canMsg.data[2] = 0xF1;            
+    tx_canMsg.data[3] = 0x01;
+    tx_canMsg.data[4] = req_time[0];
+    tx_canMsg.data[5] = req_time[1];
+    tx_canMsg.data[6] = req_time[2];
+    tx_canMsg.data[7] = 0x00;
+
+    retval = mcp2515.sendMessage(&tx_canMsg);
+    stop_timer(&timer_0xF101Wr_req);
+    start_timer(&timer_0xF101Wr_resp);
+    tstr_req = TRUE;
+  }
+
+   /****************************************************************************************************/
+  if((tstr_req != FALSE) &&
+    (get_timer(&timer_0xF101Wr_resp) < 100))
+  {
+    if (mcp2515.readMessage(&rx_canMsg) == MCP2515::ERROR_OK)
+    {   
+      if((rx_canMsg.can_id == 0x7E8) &&
+          (rx_canMsg.can_dlc == tx_canMsg.can_dlc))
+      {
+        if(rx_canMsg.data[1] == (tx_canMsg.data[1] + 0x40))
+        {
+          if((rx_canMsg.data[0] == 0x03) &&
+              ((((rx_canMsg.data[2] << 8) & 0xFF00) | 
+                ((rx_canMsg.data[3] << 0) & 0x00FF) ) == 0xF101))
+          {
+            //Wr Time success
+          }        
+        }
+      }
+    }
+  }
+  else
+  {
+    tstr_req = FALSE;
+    stop_timer(&timer_0xF101Wr_resp);   
+  }
+}
+
+/***************************************************************************************************/
+//WDBI - 0xF102
+void WDBI_0xF102()
+{
+  //Request - CPU time - every 2000 ms
+  start_timer(&timer_0xF102_req);
+  if(((get_timer(&timer_0xF102_req) > 2000) ||
+        (retval != MCP2515::ERROR_OK)) &&
+        (tstr_req == FALSE))
+  {
+    tx_canMsg.data[0] = 0x06;         
+    tx_canMsg.data[2] = 0xF1;            
+    tx_canMsg.data[3] = 0x02;
+    tx_canMsg.data[4] = req_alarm[0];
+    tx_canMsg.data[5] = req_alarm[1];
+    tx_canMsg.data[6] = req_alarm[2];
+    tx_canMsg.data[7] = 0x00;
+
+    retval = mcp2515.sendMessage(&tx_canMsg);
+    stop_timer(&timer_0xF102_req);
+    start_timer(&timer_0xF102_resp);
+    tstr_req = TRUE;
+  }
+
+   /****************************************************************************************************/
+  if((tstr_req != FALSE) &&
+    (get_timer(&timer_0xF102_resp) < 100))
+  {
+    if (mcp2515.readMessage(&rx_canMsg) == MCP2515::ERROR_OK)
+    {   
+      if((rx_canMsg.can_id == 0x7E8) &&
+          (rx_canMsg.can_dlc == tx_canMsg.can_dlc))
+      {
+        if(rx_canMsg.data[1] == (tx_canMsg.data[1] + 0x40))
+        {
+          if((rx_canMsg.data[0] == 0x03) &&
+              ((((rx_canMsg.data[2] << 8) & 0xFF00) | 
+                ((rx_canMsg.data[3] << 0) & 0x00FF) ) == 0xF102))
+          {
+            //Wr Alarm success
+          }        
+        }
+      }
+    }
+  }
+  else
+  {
+    tstr_req = FALSE;
+    stop_timer(&timer_0xF102_resp);   
+  }
+}
+
+/***************************************************************************************************/
+//WDBI - 0xF103
+void WDBI_0xF103()
+{
+  //Request - CPU time - every 2000 ms
+  start_timer(&timer_0xF103_req);
+  if(((get_timer(&timer_0xF103_req) > 2000) ||
+        (retval != MCP2515::ERROR_OK)) &&
+        (tstr_req == FALSE))
+  {
+    tx_canMsg.data[0] = 0x04;         
+    tx_canMsg.data[2] = 0xF1;            
+    tx_canMsg.data[3] = 0x03;
+    tx_canMsg.data[4] = alarm_st;
+    tx_canMsg.data[5] = 0x00;
+    tx_canMsg.data[6] = 0x00;
+    tx_canMsg.data[7] = 0x00;
+
+    retval = mcp2515.sendMessage(&tx_canMsg);
+    stop_timer(&timer_0xF103_req);
+    start_timer(&timer_0xF103_resp);
+    tstr_req = TRUE;
+  }
+
+   /****************************************************************************************************/
+  if((tstr_req != FALSE) &&
+    (get_timer(&timer_0xF103_resp) < 100))
+  {
+    if (mcp2515.readMessage(&rx_canMsg) == MCP2515::ERROR_OK)
+    {   
+      if((rx_canMsg.can_id == 0x7E8) &&
+          (rx_canMsg.can_dlc == tx_canMsg.can_dlc))
+      {
+        if(rx_canMsg.data[1] == (tx_canMsg.data[1] + 0x40))
+        {
+          if((rx_canMsg.data[0] == 0x03) &&
+              ((((rx_canMsg.data[2] << 8) & 0xFF00) | 
+                ((rx_canMsg.data[3] << 0) & 0x00FF) ) == 0xF103))
+          {
+            //Wr Alarm set/ clr success
+          }        
+        }
+      }
+    }
+  }
+  else
+  {
+    tstr_req = FALSE;
+    stop_timer(&timer_0xF102_resp);   
   }
 }
 
@@ -381,6 +579,16 @@ void loop()
       submenuopt_bypass = true;
       nanoCAN_SubMenu(nanoCAN_rotarySwt());
     }
+    else if(menu_sel == 2)
+    {
+      alarm_st = true;
+      submenu_sel = -1;
+    }
+    else if(menu_sel == 3)
+    {
+      alarm_st = false;
+      submenu_sel = -1;
+    }
     else
     {
       submenu_sel = -1;
@@ -399,5 +607,11 @@ void loop()
   tx_canMsg.data[7] = 0x00;
 
   RDBI_0xF101();
+
+  //WDBI request
+  tx_canMsg.data[1] = 0x2E;
+  WDBI_0xF101();
+  WDBI_0xF102();
+  WDBI_0xF103();
 /****************************************************************************************************/
 }
